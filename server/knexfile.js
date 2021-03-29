c87bf835434adbb78335342b38ecb29350904657
Jose <josemramirez81@outlook.com>
2021-03-28 23:04:22 -0400

    Setup kenx.js

---
+++ server/knexfile.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    client: "mysql",
+    connection: {
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "partyagile",
+        charset: "utf8"
+    }
+}
