4a8c2b3edfdbd989cc31ff8ea11a81c7f0dc278f
Jose Ramirez <josemramirez81@outlook.com>
2021-03-31 19:12:05 -0400

    Add array of colors for titles and labels

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Capstone Project</h1>
+      <h2>Capstone Project</h2>
     </div>
   );
 }
---
+++ client/src/utils/color.js
@@ -0,0 +1,5 @@
+const colors = [
+    '#102542', '#F87060' , '#52489C', '#C2EFB3', '#97ABB1', '#E57A44', '#84A98C', '#CBA135', '#9DA3A4', '#E1E6E1', '#EAC435', '#E40066', '#51CB20', '#BA9593', '#89608E', '#EDFFAB', '#FF8600', '#AEB8FE', '#370926', '#E18335', '#B3AF8F', '#DBF9F0', '#F7F9F7', '#2C3A2C', '#CBF6E9', '#904914', '#3F413A', '#EEE7EE', '#DAE7DD', '#FFE2FE'
+]
+
+export default colors;
