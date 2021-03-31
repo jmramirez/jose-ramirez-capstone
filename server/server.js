f616e67a45f1894fe3b5510531eeae05ef7856f1
Jose <josemramirez81@outlook.com>
2021-03-31 14:37:30 -0400

    Allow CORS requests

--- server/server.js
+++ server/server.js
@@ -2,8 +2,22 @@ const express = require('express')
 const path = require('path')
 const helmet = require('helmet')
 const compress = require('compression')
+const cors = require('cors')
 
 const app = express()
+
+app.use(helmet())
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+    }
+}))
+
+app.use(compress())
+app.use(cors())
 
 app.get('*', (req,res) => {
     res.send('Welcome to PartAgile')
