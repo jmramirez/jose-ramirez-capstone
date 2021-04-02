0edc9bdfa3df486c692830c7f804f740f2db0d3c
Jose <josemramirez81@outlook.com>
2021-04-02 15:40:03 -0400

    Define Task model

---
+++ server/PartyAgile.Domain/Entities/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartyAgile.Domain.Entities
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
