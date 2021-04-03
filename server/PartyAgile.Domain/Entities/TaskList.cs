b61e4287f36ad3c1dad8eb3bfe8d213115a1f989
Jose <josemramirez81@outlook.com>
2021-04-03 00:31:04 -0400

    Define TasksList model

---
+++ server/PartyAgile.Domain/Entities/TaskList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PartyAgile.Domain.Entities
+{
+    public class TasksList
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
