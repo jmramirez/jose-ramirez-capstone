3cd40cc47a587af9cb1252780551ca8062e07ce0
Jose <josemramirez81@outlook.com>
2021-04-03 01:33:02 -0400

    Create Role model

---
+++ server/PartyAgile.Domain/Entities/AppRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace PartyAgile.Domain.Entities
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        public AppRole() { }
+
+        public AppRole(string name)
+        {
+            Name = name;
+        }
+    }
+}
