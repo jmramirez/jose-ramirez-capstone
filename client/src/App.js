1471af2bd2681fb2b40254b74e951ea102ab43fb
Jose <josemramirez81@outlook.com>
2021-04-03 16:48:05 -0400

    Modify Typography files

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Capstone Project</h1>
+      <h1 className="h1">Capstone Project</h1>
       <h2>Capstone Project</h2>
+        <h3 className="h3">New Proejcts</h3>
     </div>
   );
 }
