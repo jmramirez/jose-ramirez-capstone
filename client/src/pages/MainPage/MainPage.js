68a7300ab1a8e431d29811e653883f50b3e84671
Jose <josemramirez81@outlook.com>
2021-04-03 19:09:36 -0400

    Create layout for login form

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,9 +1,28 @@
 import './MainPage.scss'
+import initialImage  from '../../assets/images/initial-page-image.jpg'
 
 const MainPage = () => {
   return(
-    <div  className="main-page">
-        <h1>This is the main page</h1>
+    <div  className="mainPage">
+        <div className="mainPage-container">
+          <div className="mainPage__image">
+
+            <div className="mainPage__image-overlay">
+                <h1 className="mainPage__heading">Party Agile</h1>
+                <p className="mainPage__sub-heading">Allow us to keep your events organize.</p>
+            </div>
+          </div>
+          <div className="mainPage__content">
+            <h2 className="mainPage__heading--secondary">Log In</h2>
+            <form className="logIn-form">
+              <label>Username/Email</label>
+              <input type="text"/>
+              <label>Password</label>
+              <input type="text"/>
+              <input type="submit"/>
+            </form>
+          </div>
+        </div>
     </div>
   )
 }
