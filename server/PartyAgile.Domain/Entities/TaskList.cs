e3fc32aba13d6573b5d07a0fdb3a769eb7a3fd78
Jose <josemramirez81@outlook.com>
2021-04-03 00:46:21 -0400

    Refactor TasList model

--- server/PartyAgile.Domain/Entities/TaskList.cs
+++ server/PartyAgile.Domain/Entities/TaskList.cs
@@ -8,6 +8,6 @@ namespace PartyAgile.Domain.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
-        public ICollection<Task> Tasks { get; set; } 
+        public ICollection<Task> Tasks { get; set; }
     }
 }
