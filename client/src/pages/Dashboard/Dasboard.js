112a76508b2c8f5b73fe303d617ae18ca6c2cb02
Jose <josemramirez81@outlook.com>
2021-04-03 17:14:34 -0400

    Define main pages

---
+++ client/src/pages/Dashboard/Dasboard.js
@@ -0,0 +1,3 @@
+const Dashboard = () => <h1>This is the dashboard page</h1>
+
+export default Dashboard;
---
+++ client/src/pages/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import './MainPage.scss'
+
+const MainPage = () => {
+  return(
+    <div  className="main-page">
+        <h1>This is the main page</h1>
+    </div>
+  )
+}
+
+export default MainPage;
