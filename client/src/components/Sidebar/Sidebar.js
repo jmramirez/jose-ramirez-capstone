0e2fb67c62043e3ce3ceee8736da5c92246e6309
Jose <josemramirez81@outlook.com>
2021-04-03 23:37:26 -0400

    Include list of events to sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -3,7 +3,17 @@ import './Sidebar.scss'
 const Sidebar = () => {
   return(
     <div className="sidebar">
-      <h1>This is a sidebar</h1>
+      <div className="sidebar-header">
+        <h1 className="sidebar-header__title">Party Agile</h1>
+      </div>
+      <div className="sidebar-events">
+        <h3 className="sidebar-events__title">List of Events</h3>
+        <ul className="events">
+          <li><p>My Wedding</p></li>
+          <li><p>Pandemic Birthday</p></li>
+          <li><p>Picnic with my friends</p></li>
+          </ul>
+      </div>
     </div>
   )
 }
