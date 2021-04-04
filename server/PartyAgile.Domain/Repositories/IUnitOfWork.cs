5bc538f6572c1cac7fdc8c1a6d4d3866bf86e993
Jose <josemramirez81@outlook.com>
2021-04-04 19:27:56 -0400

    Define the methods used to save changes to the database

---
+++ server/PartyAgile.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<bool> SaveEntitiesASync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
