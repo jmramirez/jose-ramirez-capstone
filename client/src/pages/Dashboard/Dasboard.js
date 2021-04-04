8ea4d06290eff72e5aa53f7d8cb452b593d3801b
Jose <josemramirez81@outlook.com>
2021-04-04 12:46:25 -0400

    Define AddVendorForm

--- client/src/pages/Dashboard/Dasboard.js
+++ client/src/pages/Dashboard/Dasboard.js
@@ -2,13 +2,14 @@ import Sidebar from '../../components/Sidebar/Sidebar'
 import './Dashboard.scss'
 import Event from '../../components/Event/Event'
 import AddEventForm from '../../components/AddEventForm/AddEventForm'
+import AddVendorForm from '../../components/AddVendorForm/AddVendorForm'
 
 const Dashboard = () => {
   return(
     <div className="dashboard">
       <Sidebar />
       <div className="dashboard-content">
-        <AddEventForm />
+        <AddVendorForm />
       </div>
     </div>
   )
