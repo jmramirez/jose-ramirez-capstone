b2dbc5b86238ae93e20b25f7f12296262753bee9
Jose <josemramirez81@outlook.com>
2021-04-04 01:24:10 -0400

    Create AddEventForm component

---
+++ client/src/components/AddEventForm/AddEventForm.js
@@ -0,0 +1,11 @@
+import './AddEventForm.scss'
+
+const AddEventForm = () => {
+  return(
+    <main className="add-event">
+      <h1>This is the form</h1>
+    </main>
+  )
+}
+
+export default AddEventForm;
--- client/src/pages/Dashboard/Dasboard.js
+++ client/src/pages/Dashboard/Dasboard.js
@@ -1,12 +1,13 @@
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './Dashboard.scss'
 import Event from '../../components/Event/Event'
+import AddEventForm from '../../components/AddEventForm/AddEventForm'
 
 const Dashboard = () => {
   return(
     <div className="dashboard">
       <Sidebar />
-      <Event />
+      <AddEventForm />
     </div>
   )
 }
