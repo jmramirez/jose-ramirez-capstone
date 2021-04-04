97bd7017f7792400e331a91f525c14da51268be8
Jose <josemramirez81@outlook.com>
2021-04-03 23:06:55 -0400

    Create Event component

---
+++ client/src/components/Event/Event.js
@@ -0,0 +1,10 @@
+import './Event.scss'
+
+const Event = () => {
+  return(
+    <div className="event" >
+      <h1>This is the event component</h1>
+    </div>
+  )
+}
+export default Event
--- client/src/pages/Dashboard/Dasboard.js
+++ client/src/pages/Dashboard/Dasboard.js
@@ -1,10 +1,12 @@
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './Dashboard.scss'
+import Event from '../../components/Event/Event'
 
 const Dashboard = () => {
   return(
     <div className="dashboard">
       <Sidebar />
+      <Event />
     </div>
   )
 }
