12e7790e18cb79abc6521df83a5b541ef5544c88
Jose <josemramirez81@outlook.com>
2021-04-05 10:07:31 -0400

    Define models repositories interface

---
+++ server/PartyAgile.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using PartyAgile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Repositories
+{
+    public interface ICommentRepository : IRepository
+    {
+        Task<IEnumerable<Comment>> GetCommentByTaskIdAsync();
+        Task<Comment> GetAsync(Guid Id);
+        Comment Add(Comment commemt);
+
+    }
+}
---
+++ server/PartyAgile.Domain/Repositories/IVendorRepository.cs
@@ -0,0 +1,16 @@
+﻿using PartyAgile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Repositories
+{
+    public interface IVendorRepository
+    {
+        Task<Vendor> GetAsync(Guid id);
+        Vendor Add(Vendor item);
+        Vendor Update(Vendor item);
+    }
+}
