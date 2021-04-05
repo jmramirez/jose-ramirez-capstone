a38e7b2a86e8192a14b4649a8361418178491b96
Jose <josemramirez81@outlook.com>
2021-04-05 17:19:52 -0400

    Create Vendor mapper

---
+++ server/PartyAgile.Domain/Mappers/VendorMapper.cs
@@ -0,0 +1,97 @@
+﻿using PartyAgile.Domain.Entities;
+using PartyAgile.Domain.Requests.Vendor;
+using PartyAgile.Domain.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Mappers
+{
+    public interface IVendorMapper
+    {
+        Vendor Map(AddVendorRequest request);
+        Vendor Map(EditVendorRequest request);
+        VendorResponse Map(Vendor request);
+    }
+
+    public class VendorMapper : IVendorMapper
+    {
+        public Vendor Map(AddVendorRequest request)
+        {
+            if (request == null) return null;
+
+            var vendorItem = new Vendor
+            {
+               Name = request.Name,
+               Type = request.Type,
+               ContactName = request.ContactName,
+               ContactEmail = request.ContactEmail,
+               Address = request.Address,
+            };
+
+            if(request.Budget != null)
+            {
+                vendorItem.Budget = new Price { Amount = request.Budget.Amount, Currency = request.Budget.Currency };
+            }
+
+            if (request.DepositPaid != null)
+            {
+                vendorItem.DepositPaid = new Price { Amount = request.DepositPaid.Amount, Currency = request.DepositPaid.Currency };
+            }
+
+            return vendorItem;
+        }
+
+        public Vendor Map(EditVendorRequest request)
+        {
+            if (request == null) return null;
+
+            var vendorItem = new Vendor
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Type = request.Type,
+                ContactName = request.ContactName,
+                ContactEmail = request.ContactEmail,
+                Address = request.Address,
+            };
+
+            if (request.Budget != null)
+            {
+                vendorItem.Budget = new Price { Amount = request.Budget.Amount, Currency = request.Budget.Currency };
+            }
+
+            if (request.DepositPaid != null)
+            {
+                vendorItem.DepositPaid = new Price { Amount = request.DepositPaid.Amount, Currency = request.DepositPaid.Currency };
+            }
+
+            return vendorItem;
+        }
+
+        public VendorResponse Map(Vendor vendorItem)
+        {
+            var response = new VendorResponse
+            {
+                Id = vendorItem.Id,
+                Name = vendorItem.Name,
+                Type = vendorItem.Type,
+                ContactName = vendorItem.ContactName,
+            };
+
+            if (vendorItem.Budget != null)
+            {
+                vendorItem.Budget = new Price { Amount = vendorItem.Budget.Amount, Currency = vendorItem.Budget.Currency };
+            }
+
+            if (vendorItem.DepositPaid != null)
+            {
+                vendorItem.DepositPaid = new Price { Amount = vendorItem.DepositPaid.Amount, Currency = vendorItem.DepositPaid.Currency };
+            }
+
+            return response;
+        }
+    }
+}
