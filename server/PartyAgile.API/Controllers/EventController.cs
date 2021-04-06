b01ccd806f90b0b7853bb6b32f29d2954e66e18e
Jose <josemramirez81@outlook.com>
2021-04-05 20:04:57 -0400

    Define EventController

---
+++ server/PartyAgile.API/Controllers/EventController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartyAgile.Domain.Requests.Event;
+using PartyAgile.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyAgile.API.Controllers
+{
+    [Route("api/events")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        } 
+    }
+}
