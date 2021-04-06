f2eeebf2331aad6a72eed16fbdf420747b95113b
Jose <josemramirez81@outlook.com>
2021-04-05 20:53:41 -0400

    Define VendorController

---
+++ server/PartyAgile.API/Controllers/VendorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartyAgile.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyAgile.API.Controllers
+{
+    [Route("api/vendor")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly IVendorService _vendorService;
+
+        public VendorController(IVendorService vendorService)
+        {
+            _vendorService = vendorService;
+        }
+
+       
+    }
+}
