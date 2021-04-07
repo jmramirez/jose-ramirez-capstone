0d3a5f49b4b89ec69bbdd6556cee986f2353117e
Jose <josemramirez81@outlook.com>
2021-04-07 04:54:17 -0400

    Refactor Dashboard component

--- client/src/pages/Dashboard/Dasboard.js
+++ client/src/pages/Dashboard/Dasboard.js
@@ -1,13 +1,7 @@
 import { Component } from 'react'
 import axios from 'axios'
-import Sidebar from '../../components/Sidebar/Sidebar'
 import './Dashboard.scss'
 import  { url } from '../../config'
-import Event from '../../components/Event/Event'
-import AddEventForm from '../../components/AddEventForm/EventsForm'
-import AddVendorForm from '../../components/AddVendorForm/AddVendorForm'
-import TaskModal from '../../components/TaskModal/TaskModal'
-import Vendor from '../../components/Vendor/Vendor'
 import VendorList from '../../components/VendorsList/VendorsList'
 import EventDetails from '../../components/EventDetails/EventDetails'
 
@@ -25,6 +19,12 @@ class Dashboard extends Component {
           this.setState({
             eventSelected: response.data
           })
+          return axios.get(`${url}events/${this.props.match.params.id}/vendors`)
+        })
+        .then(response =>{
+          this.setState({
+              vendors: response.data
+          })
         })
     }
   }
@@ -40,6 +40,12 @@ class Dashboard extends Component {
           this.setState({
             eventSelected: response.data
           })
+          return axios.get(`${url}events/${this.props.match.params.id}/vendors`)
+        })
+        .then(response =>{
+          this.setState({
+            vendors: response.data
+          })
         })
     }
   }
@@ -50,15 +56,13 @@ class Dashboard extends Component {
     return(
       <div className="dashboard">
         <div className="dashboard-content">
-          {/*{(this.props.match.params.action && this.props.match.params.action === "add" && <AddEventForm  action="Add New"/>)}*/}
           {(this.state.eventSelected) && <EventDetails eventItem={this.state.eventSelected} />}
           {(this.state.vendors.length === 0)?
             <>
-
+              <p className="dashboard__no-vendors">There is not Vendors Assigned to this Event</p>
             </>
             :
             <>
-              <EventDetails eventItem={this.state.eventSelected}/>
               <VendorList vendors={this.state.vendors} />
             </>
 
