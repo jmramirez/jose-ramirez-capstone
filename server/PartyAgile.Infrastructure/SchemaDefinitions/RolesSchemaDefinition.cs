7af7233a4518a6c51d97344e6a2f745a04d8e0b2
Jose Ramirez <josemramirez81@outlook.com>
2021-09-21 14:03:51 -0400

    Modify roles name

---
+++ server/PartyAgile.Infrastructure/SchemaDefinitions/RolesSchemaDefinition.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PartyAgile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Infrastructure.SchemaDefinitions
+{
+    public class RolesSchemaDefinition : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.HasData(
+                new AppRole
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Planner"
+                },
+                new AppRole
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Vendor"
+                }
+            );
+        }
+    }
+}
