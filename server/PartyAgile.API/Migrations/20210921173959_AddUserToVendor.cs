6ce53bbdb27f638738dd2cb7c1d6eaff2f4f44ea
Jose Ramirez <josemramirez81@outlook.com>
2021-09-21 14:09:28 -0400

    Create migration to associate User Entity with Vendor entity

---
+++ server/PartyAgile.API/Migrations/20210921173959_AddUserToVendor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PartyAgile.API.Migrations
+{
+    public partial class AddUserToVendor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VendorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                schema: "partyagile",
+                table: "Vendors",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendors_UserId",
+                schema: "partyagile",
+                table: "Vendors",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendors_AspNetUsers_UserId",
+                schema: "partyagile",
+                table: "Vendors",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendors_AspNetUsers_UserId",
+                schema: "partyagile",
+                table: "Vendors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vendors_UserId",
+                schema: "partyagile",
+                table: "Vendors");
+
+            
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                schema: "partyagile",
+                table: "Vendors");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "VendorId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+        }
+    }
+}
