13bca040785885402e93dd4b69f15e356dbea576
Jose Ramirez <josemramirez81@outlook.com>
2021-09-21 14:05:53 -0400

    Setup Identity Framework Password options

--- server/PartyAgile.API/Startup.cs
+++ server/PartyAgile.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -51,6 +52,13 @@ namespace PartyAgile.API
             });
             
             services.AddHttpClient();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
             
         }
 
