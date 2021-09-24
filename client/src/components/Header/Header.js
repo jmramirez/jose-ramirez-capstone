e818bdc3ea4cb162a6327e6d01ed7f615292f0ad
Jose Ramirez <josemramirez81@outlook.com>
2021-09-24 00:40:43 -0400

    Modify code to include user authentication

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,17 +2,17 @@ import './Header.scss'
 import { Link } from 'react-router-dom';
 
 
-export const Header = () => (
+export const Header = ({ handleLogout, user }) => (
   <header className="header">
     <nav className="header__nav">
       <Link to='/' className="header__nav__heading-link">
         Party Agile
       </Link>
       <div className="header__nav__right">
-        <p className="header__nav__right-user">Jose Ramirez</p>
+        {user && <p className="header__nav__right-user">{user.name}</p>}
         <div className="header__nav__right-actions">
           <Link to="/" className="header__nav__right__link">Events</Link>
-          <Link to="/" className="header__nav__right__link">Logout</Link>
+          <button className="header__nav__right__button" onClick={handleLogout}>Logout</button>
         </div>
       </div>
     </nav>
