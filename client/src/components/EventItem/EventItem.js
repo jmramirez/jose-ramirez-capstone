919f8889b321ab4eb2c71971da326ad7e62fa978
Jose Ramirez <josemramirez81@outlook.com>
2021-09-26 11:40:18 -0400

    Refactor event item component layout

--- client/src/components/EventItem/EventItem.js
+++ client/src/components/EventItem/EventItem.js
@@ -1,9 +1,25 @@
 import './EventItem.scss'
 import {Link} from 'react-router-dom'
 
+/*
 export const EventItem = ({ event }) => (
   <Link to={`/getevent/${event.id}`} className="event-item">
     <p className="event-item-title-title">{event.title}</p>
     <p className="event-item-date">Date: { new Date(event.eventDate).toLocaleDateString()}</p>
   </Link>
+)*/
+
+
+export const EventItem = ({ event }) => (
+  <div className="eventItem">
+    <Link to="/" className="eventItem__header">{event.title}</Link>
+    <div className="eventItem__content">
+      <p className="eventItem__content__text--description"><span className="eventItem__content__label">Description: </span>{event.description}</p>
+      <p className="eventItem__content__text--date"><span className="eventItem__content__label">Event Date: </span>{new Date(event.eventDate).toLocaleDateString()}</p>
+      <p className="eventItem__content__text"><span className="eventItem__content__label">Budget: </span>{event.budget.amount}{event.budget.currency}</p>
+      <p className="eventItem__content__text">
+        <span className="eventItem__content__label">Guests Aprox*: </span>{event.guests} {event.guests > 1 ? 'people' :'person'}
+      </p>
+    </div>
+  </div>
 )
