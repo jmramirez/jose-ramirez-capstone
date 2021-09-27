e1a488fe1db15b6ecac8517a639eecae956dd258
Jose Ramirez <josemramirez81@outlook.com>
2021-09-27 13:20:26 -0400

    Refactor code to remove event detail info component from page

--- client/src/pages/EventsPage/EventsPage.js
+++ client/src/pages/EventsPage/EventsPage.js
@@ -1,12 +1,42 @@
 import './EventsPage.scss'
 import {PageHeader} from '../../components/PageHeader/PageHeader';
 import {EventsList} from '../../components/EventsList/EventsList';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {url} from '../../config';
+import EventDetails from '../../components/EventDetails/EventDetails';
 
-export const EventsPage = ({ events }) => (
-  <div className="events">
-    <PageHeader elementType="Events" buttonText="Create New Event"/>
-    <div className="events-content">
-      <EventsList events={events} />
+export const EventsPage = ({ events, match, authenticated }) => {
+  const eventId = match.params.eventId
+  const [event, setEvent] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+
+
+  useEffect(() => {
+    const getEvent = (eventId) => {
+      axios.get(`${url}events/${eventId}`,{
+        headers: {
+          'Authorization' : `Bearer ${authenticated}`
+        }
+      })
+        .then(response => {
+          console.log(response.data)
+          setEvent(response.data)
+          setLoading(false)
+        })
+    }
+    if(eventId){
+      getEvent(eventId)
+    }
+  }, [eventId])
+
+  return(
+    <div className="events">
+      <PageHeader elementType="Events" buttonText="Create New Event"/>
+      <div className="events-content">
+        <EventsList events={events} />
+      </div>
     </div>
-  </div>
-)
+  )
+}
