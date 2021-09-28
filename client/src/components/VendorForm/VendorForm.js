ea2d6d57f1a7e5c0ef6aaded9c9f0e1022cb097c
Jose Ramirez <josemramirez81@outlook.com>
2021-09-28 16:34:50 -0400

    Include input validation

--- client/src/components/VendorForm/VendorForm.js
+++ client/src/components/VendorForm/VendorForm.js
@@ -1,28 +1,45 @@
-import { Component } from 'react'
 import  { url } from '../../config'
-
-
 import './VendorForm.scss'
 import axios from 'axios'
-import {VendorsSelect} from '../VendorsSelect/VendorsSelect'
 import {useForm} from 'react-hook-form'
 import { useState, useEffect } from 'react'
 import {Link, useHistory} from 'react-router-dom'
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import {Icon} from '../Icon/Icon';
+
+const schema = yup.object().shape({
+  type: yup.string().required("Vendor type is required"),
+  name:  yup.string().required("Vendor Name is required"),
+  contactEmail:  yup.string().required("Vendor Email is required"),
+  budget: yup.number().typeError('Budget is required, should be a number').min(0, "Budget should be greater than 0"),
+  depositPaid: yup.number().typeError('Deposit Paid is required, should be a number').min(0, "Deposit Paid should be greater than 0"),
+})
 
 
 export const VendorForm = ({match , action, children, handleUpdate, user }) => {
   const [addNew, setAddNew] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [vendors, setVendors] = useState([])
   const [vendorId, setVendorId] = useState('')
-  const { register, handleSubmit, formState: { errors}, setValue} = useForm()
+  const { register, handleSubmit, formState: { errors }, setValue} = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema)
+  })
   const [selected, setSelected] = useState(false)
   const history = useHistory()
 
   useEffect(() => {
+    console.log('here')
     axios.get(`${url}vendor`)
       .then(response => {
-        setVendors(response.data)
+        if(response.data.length){
+          setVendors(response.data)
+        }
+        else {
+          setAddNew(true)
+        }
+        setLoading(false)
       })
   }, [])
 
@@ -108,7 +125,7 @@ export const VendorForm = ({match , action, children, handleUpdate, user }) => {
   }
 
   const handleClick =() => {
-    history.push(`/getevent/${children.params.eventId}`)
+    history.goBack()
   }
 
   const handleNew = () => {
@@ -121,18 +138,17 @@ export const VendorForm = ({match , action, children, handleUpdate, user }) => {
   }
 
   return(
+    loading ? <h2>Loading...</h2>
+      :
     <main className="vendorForm">
       <div className="vendorForm__header">
         <h2>{action} Vendor</h2>
         {!addNew && action==="Add" &&(
-          <button className="vendorForm__header__action" onClick={handleNew}>
+          <button className="vendorForm__header__action" onClick={handleNew} type="button">
             <Icon name="add" />New <span className="vendorForm__header__action__text">&nbsp;Vendor</span>
           </button>
         )}
       </div>
-
-
-
       <form className="vendorForm__form" onSubmit={handleSubmit(onSubmit)}>
         {(!addNew && action ==="Add") ?
           <div className="vendorForm__form__existing">
@@ -140,11 +156,12 @@ export const VendorForm = ({match , action, children, handleUpdate, user }) => {
               id="vendors"
               name="vendors"
               value={vendors.id}
+              defaultValue="Please Select Vendor"
               className="vendorForm__form__existing__select"
               onChange={handleChange}
               {...register("vendorId")}
             >
-              <option value="" disabled selected hidden>Please Select Vendor</option>
+              <option disabled value="Please Select Vendor">Please Select Vendor</option>
               {vendors.map(vendor =>(
                 <option value={vendor.id}>Name: {vendor.name} </option>
               ))}
@@ -165,11 +182,13 @@ export const VendorForm = ({match , action, children, handleUpdate, user }) => {
         <div className="vendorForm__form__row">
           <div className="vendorForm__form__row-controls">
             <label className="vendorForm__form__row-controls__label">Vendor Type</label>
-            <input className="vendorForm__form__row-controls__input" type="text" name={'type'} {...register("type")} autoComplete="off"/>
+            <input className="vendorForm__form__row-controls__input" type="text" name={'type'} {...register("type",{required:true})} autoComplete="off"/>
+            {errors.type &&<p className="vendorForm__error"><span className="material-icons eventsForm__error-icon">error</span>{errors.type?.message}</p>}
           </div>
           <div className="vendorForm__form__row-controls">
             <label className="vendorForm__form__row-controls__label">Vendor Name</label>
-            <input className="vendorForm__form__row-controls__input" type="text" name={'name'}  {...register("name")} autoComplete="off"/>
+            <input className="vendorForm__form__row-controls__input" type="text" name={'name'}  {...register("name",{required:true})} autoComplete="off"/>
+            {errors.name &&<p className="vendorForm__error"><span className="material-icons eventsForm__error-icon">error</span>{errors.name?.message}</p>}
           </div>
         </div>
         <div className="vendorForm__form__row">
@@ -179,7 +198,8 @@ export const VendorForm = ({match , action, children, handleUpdate, user }) => {
           </div>
           <div className="vendorForm__form__row-controls">
             <label className="vendorForm__form__row-controls__label">Contact Email</label>
-            <input className="vendorForm__form__row-controls__input" type="text" name={'contactEmail'} {...register("contactEmail")} autoComplete="off"/>
+            <input className="vendorForm__form__row-controls__input" type="text" name={'contactEmail'} {...register("contactEmail", {required: true})} autoComplete="off"/>
+            {errors.contactEmail &&<p className="vendorForm__error"><span className="material-icons eventsForm__error-icon">error</span>{errors.contactEmail?.message}</p>}
           </div>
         </div>
         <div className="vendorForm__form__row vendorForm__form__row--text">
@@ -190,16 +210,18 @@ export const VendorForm = ({match , action, children, handleUpdate, user }) => {
           <div className="vendorForm__form__row-controls">
             <label className="vendorForm__form__row-controls__label">Budget</label>
             <input className="vendorForm__form__row-controls__input" type="text" name={'budget'}  {...register("budget")} autoComplete="off"/>
+            {errors.budget &&<p className="vendorForm__error"><span className="material-icons eventsForm__error-icon">error</span>{errors.budget?.message}</p>}
           </div>
           <div className="vendorForm__form__row-controls">
             <label className="vendorForm__form__row-controls__label">Deposit Paid</label>
             <input className="vendorForm__form__row-controls__input" type="text"  name={'depositPaid'}  {...register("depositPaid")} autoComplete="off"/>
+            {errors.depositPaid &&<p className="vendorForm__error"><span className="material-icons eventsForm__error-icon">error</span>{errors.depositPaid?.message}</p>}
           </div>
         </div>
           </>
           )}
         <div className="vendorForm__form__actions">
-          <button onClick={handleClick} className="vendorForm__form__actions__button">Cancel</button>
+          <button onClick={handleClick} className="vendorForm__form__actions__button" type="button">Cancel</button>
           <button className="vendorForm__form__actions__button">
             <Icon name="add" />
             {action} Vendor
