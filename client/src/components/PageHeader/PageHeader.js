0b6e92b3cdf1e4641f016b04367a145d5341595c
Jose Ramirez <josemramirez81@outlook.com>
2021-10-05 18:00:27 -0400

    Refactor PageHeader layout for Planner view

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -2,7 +2,7 @@ import './PageHeader.scss'
 import { Link } from 'react-router-dom';
 import { Icon } from '../Icon/Icon';
 
-export const PageHeader = ({ elementType, buttonText, handleChange, role}) => {
+export const PageHeader = ({ elementType, buttonText, handleChange, user}) => {
 
 
   const handleTiming = (e) => {
@@ -10,24 +10,31 @@ export const PageHeader = ({ elementType, buttonText, handleChange, role}) => {
   }
 
   return (
-    <div className="pageHeader">
-      <div className="pageHeader__header">
-        <h2 className="pageHeader__header-heading">
-          {role && role === 'Planner' && `List of ${elementType}`}
-          {role && role === 'Vendor' && 'Events Assigned'}
-        </h2>
-        <select className="pageHeader__header-select" id="events" defaultValue="newEvents" onChange={handleTiming}>
-          <option value="newEvents" className="pageHeader__header-select-option">Next Events</option>
-          <option value="oldEvents" className="pageHeader__header-select-option">Past Events</option>
-        </select>
-      </div>
-      {role && role === 'Planner' && (
-        <div className="pageHeader__actions">
-          <Link to={`/${elementType.toLowerCase()}/addEvent`} className="pageHeader__actions-link">
-            <Icon name="add"/><p className="pageHeader__actions-link__text">{buttonText}</p>
-          </Link>
+    <div className={"pageHeader" + (user && user.role==='Vendor' ?' pageHeader--vendor' : '')}>
+      <select className="pageHeader__header-select" id="events" defaultValue="newEvents" onChange={handleTiming}>
+        <option value="newEvents" className="pageHeader__header-select-option">Next Events</option>
+        <option value="oldEvents" className="pageHeader__header-select-option">Past Events</option>
+      </select>
+      {user && user.role === 'Vendor' &&(
+        <div className="pageHeader__vendor-info">
+          <p><span>Vendor Name:</span> {user.vendor.name}</p>
+          <p><span>Vendor Type:</span> {user.vendor.type}</p>
         </div>
       )}
+      <div className="pageHeader__header">
+        <h2 className="pageHeader__header-heading">
+          {user && user.role === 'Planner' && `List of ${elementType}`}
+          {user && user.role === 'Vendor' && 'Events Assigned'}
+        </h2>
+        {user && user.role === 'Planner' && (
+          <div className="pageHeader__actions">
+            <Link to={`/${elementType.toLowerCase()}/addEvent`} className="pageHeader__actions-link">
+              <Icon name="add"/><p className="pageHeader__actions-link__text">{buttonText}</p>
+            </Link>
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
