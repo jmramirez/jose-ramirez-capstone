cc8608b462116b29dd3cba858704f38ebe3c8ec8
Jose Ramirez <josemramirez81@outlook.com>
2021-10-06 14:31:04 -0400

    Refactor components layout to show Vendors information

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -11,10 +11,22 @@ export const PageHeader = ({ elementType, buttonText, handleChange, user}) => {
 
   return (
     <div className={"pageHeader" + (user && user.role==='Vendor' ?' pageHeader--vendor' : '')}>
-      <select className="pageHeader__header-select" id="events" defaultValue="newEvents" onChange={handleTiming}>
-        <option value="newEvents" className="pageHeader__header-select-option">Next Events</option>
-        <option value="oldEvents" className="pageHeader__header-select-option">Past Events</option>
-      </select>
+      <div className="pageHeader__actions">
+        <select className="pageHeader__header-select" id="events" defaultValue="newEvents" onChange={handleTiming}>
+          <option value="newEvents" className="pageHeader__header-select-option">Next Events</option>
+          <option value="oldEvents" className="pageHeader__header-select-option">Past Events</option>
+        </select>
+        {user && user.role === 'Planner' && (
+          <Link to={`/${elementType.toLowerCase()}/addEvent`} className="pageHeader__actions-link">
+            <Icon name="add"/><p className="pageHeader__actions-link__text">{buttonText}</p>
+          </Link>
+        )}
+        {user && user.role === 'Vendor' &&(
+          <Link to={`/${elementType.toLowerCase()}/addEvent`} className="pageHeader__actions-link">
+            <Icon name="mode_edit"/><p className="pageHeader__actions-link__text">{buttonText}</p>
+          </Link>
+        )}
+      </div>
       <div className={"pageHeader__header" + (user && user.role==='Vendor' ?' pageHeader__header--vendor' : '')}>
         <h2 className="pageHeader__header-heading">
           {user && user.role === 'Planner' && `List of ${elementType}`}
@@ -24,13 +36,6 @@ export const PageHeader = ({ elementType, buttonText, handleChange, user}) => {
           <div className="pageHeader__vendor-info">
             <p className="pageHeader__vendor-info__label"><span className="pageHeader__vendor-info__label--heading">Vendor Name:</span> {user.vendor.name}</p>
             <p className="pageHeader__vendor-info__label"><span className="pageHeader__vendor-info__label--heading">Vendor Type:</span> {user.vendor.type}</p>
-          </div>
-        )}
-        {user && user.role === 'Planner' && (
-          <div className="pageHeader__actions">
-            <Link to={`/${elementType.toLowerCase()}/addEvent`} className="pageHeader__actions-link">
-              <Icon name="add"/><p className="pageHeader__actions-link__text">{buttonText}</p>
-            </Link>
           </div>
         )}
       </div>
--- client/src/pages/EventsPage/EventsPage.js
+++ client/src/pages/EventsPage/EventsPage.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 import {url} from '../../config';
 import EventDetails from '../../components/EventDetails/EventDetails';
 
-export const EventsPage = ({ events, match, authenticated, handleChange, role }) => {
+export const EventsPage = ({ events, match, authenticated, handleChange, user }) => {
   const eventId = match.params.eventId
   const [event, setEvent] = useState(null)
   const [loading, setLoading] = useState(true)
@@ -33,9 +33,9 @@ export const EventsPage = ({ events, match, authenticated, handleChange, role })
 
   return(
     <div className="events">
-      <PageHeader elementType="Events" buttonText="Create New Event" handleChange={handleChange} role={role}/>
+      <PageHeader elementType="Events" buttonText={(user && user.role ==='Planner')?"Create New Event":"Edit Vendor Info"} handleChange={handleChange} user={user}/>
       <div className="events-content">
-        <EventsList events={events} />
+        <EventsList events={events} user={user} />
       </div>
     </div>
   )
