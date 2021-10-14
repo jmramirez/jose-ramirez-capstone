c9d4969a7d3d1aa27a27799a4e9ab18608aa8a5c
Jose Ramirez <josemramirez81@outlook.com>
2021-10-14 12:56:30 -0400

    Create migration for Message entity

--- server/PartyAgile.API/Migrations/20211013075054_AddMessages.Designer.cs
+++ server/PartyAgile.API/Migrations/20211013075054_AddMessages.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PartyAgile.Infrastructure;
 
 namespace PartyAgile.API.Migrations
 {
     [DbContext(typeof(PartyAgileDbContext))]
-    partial class PartyAgileDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211013075054_AddMessages")]
+    partial class AddMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/PartyAgile.API/Migrations/20211013075054_AddMessages.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PartyAgile.API.Migrations
+{
+    public partial class AddMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                schema: "partyagile",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Sender = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VendorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_EventVendors_EventId_VendorId",
+                        columns: x => new { x.EventId, x.VendorId },
+                        principalSchema: "partyagile",
+                        principalTable: "EventVendors",
+                        principalColumns: new[] { "EventId", "VendorId" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_EventId_VendorId",
+                schema: "partyagile",
+                table: "Messages",
+                columns: new[] { "EventId", "VendorId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages",
+                schema: "partyagile");
+
+            
+        }
+    }
+}
