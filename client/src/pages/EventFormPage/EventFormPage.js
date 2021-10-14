79026eaae5ca56e14207fe3581b688e551144130
Jose Ramirez <josemramirez81@outlook.com>
2021-10-13 21:43:41 -0400

    Include option to link Message for container

--- client/src/pages/EventFormPage/EventFormPage.js
+++ client/src/pages/EventFormPage/EventFormPage.js
@@ -1,12 +1,16 @@
 import './EventFormPage.scss'
 import { EventForm }  from '../../components/EventForm/EventForm'
 import {VendorForm} from '../../components/VendorForm/VendorForm';
+import {PaymentForm} from '../../components/PaymentForm/PaymentForm';
+import {MessageForm} from '../../components/MessageForm/MessageForm';
 
 const EventFormPage = ({ action, match, history, handleUpdate, authenticated, elementType }) => {
   return(
     <section className="form-section">
       {elementType === 'event' && (<EventForm action={(action ==="add")? "Add New" : "Edit"} history={history} match={match} handleUpdate={handleUpdate} authenticated={authenticated}/>)}
-      {elementType === 'vendor' &&(<VendorForm action={(action ==="add")? "Add" : "Edit"} match={match} authenticated={authenticated} />)}
+      {elementType === 'vendor' &&(<VendorForm action={(action ==="add")? "Add" : "Edit"} match={match} authenticated={authenticated} handleUpdate={handleUpdate}/>)}
+      {elementType === 'vendorEvent' && (<PaymentForm match={match} authenticated={authenticated} handleUpdate={handleUpdate} />)}
+      {action === 'message' && <MessageForm />}
     </section>
   )
 }
