0242f8cf7c07a490d05144b9a96a4f619f2f6589
Jose Ramirez <josemramirez81@outlook.com>
2021-10-15 23:41:05 -0400

    Define Messages DTOs

---
+++ server/PartyAgile.Domain/Requests/Message/AddMessageResquest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Requests.Message
+{
+    public class AddMessageResquest
+    {
+        public string Content { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public Guid VendorId { get; set; }
+        public Guid EventId { get; set; }
+    }
+}
---
+++ server/PartyAgile.Domain/Responses/MessageResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Responses
+{
+    public class MessageResponse
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string SenderName { get; set; }
+    }
+}
