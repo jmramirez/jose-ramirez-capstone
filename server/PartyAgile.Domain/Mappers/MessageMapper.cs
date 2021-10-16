7948572919773d491c8ce226c38ad7bcc0031f53
Jose Ramirez <josemramirez81@outlook.com>
2021-10-15 23:42:01 -0400

    Define mapper to translate message data transfer objects into message entity objects

---
+++ server/PartyAgile.Domain/Mappers/MessageMapper.cs
@@ -0,0 +1,46 @@
+﻿using PartyAgile.Domain.Entities;
+using PartyAgile.Domain.Requests.Message;
+using PartyAgile.Domain.Responses;
+
+namespace PartyAgile.Domain.Mappers
+{
+    public interface IMessageMapper
+    {
+        Message Map(AddMessageResquest request);
+        MessageResponse Map(Message request);
+    }
+
+
+    public class MessageMapper : IMessageMapper
+    {
+        public Message Map(AddMessageResquest request)
+        {
+            if (request == null) return null;
+
+            var messageItem = new Message
+            {
+                Content = request.Content,
+                Created = request.Created,
+                VendorId = request.VendorId,
+                EventId = request.EventId
+            };
+
+            return messageItem;
+        }
+
+        public MessageResponse Map(Message request)
+        {
+            if (request == null) return null;
+
+            var response = new MessageResponse
+            {
+                Id = request.Id,
+                Content = request.Content,
+                Created = request.Created,
+                SenderName = request.SenderName
+            };
+
+            return response;
+        }
+    }
+}
