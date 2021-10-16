5968f4f1069bbdd7f42338804093d9e06bdffacc
Jose Ramirez <josemramirez81@outlook.com>
2021-10-16 15:54:30 -0400

    Create methods to allow real time communication using SignalR

---
+++ server/PartyAgile.API/Hubs/MessagesHubs.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyAgile.API.Hubs
+{
+    public class MessagesHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            await Clients.Caller.SendAsync("Message", "Successfully connected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.Caller.SendAsync("Message", "Successfully disconnected");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SubscribeEvent(Guid eventId, Guid vendorId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"Chat-{eventId}-{vendorId}");
+            await Clients.Caller.SendAsync("Message", "Successfully subcribed");
+        }
+
+        public async Task UnsubscribeEvent(Guid eventId, Guid vendorId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"Chat-{eventId}-{vendorId}");
+            await Clients.Caller.SendAsync("Message", "Successfully unsubcribed");
+        }
+    }
+}
