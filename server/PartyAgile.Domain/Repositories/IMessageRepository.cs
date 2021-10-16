ea6b5b5f1d010163a8212897b80acc429e7ef73c
Jose Ramirez <josemramirez81@outlook.com>
2021-10-16 15:46:25 -0400

    Define and implemente MessageRepository with methods to add and get messages

---
+++ server/PartyAgile.Domain/Repositories/IMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using PartyAgile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Repositories
+{
+    public interface IMessageRepository : IRepository
+    {
+        Message Add(Message item);
+        Task<IEnumerable<Message>> GetMessagesAsync(Guid vendorId, Guid eventId);
+    }
+}
---
+++ server/PartyAgile.Infrastructure/Repositories/MessageRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartyAgile.Domain.Entities;
+using PartyAgile.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Infrastructure.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly PartyAgileDbContext _context;
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public MessageRepository(PartyAgileDbContext context)
+        {
+            _context = context ?? throw new ArgumentException(nameof(context));
+        }
+
+        public Message Add(Message messageItem)
+        {
+            return _context.Messages
+                .Add(messageItem).Entity;
+        }
+
+        public async Task<IEnumerable<Message>> GetMessagesAsync(Guid vendorId, Guid eventId)
+        {
+            var now = new DateTimeOffset(DateTime.Now);
+            var messages = await _context.Messages
+                .Where(e => e.EventId == eventId)
+                .Where(v => v.VendorId == vendorId)
+                .OrderByDescending(d => d.Created)
+                .ToListAsync();
+
+            return messages;
+        }
+    }
+}
