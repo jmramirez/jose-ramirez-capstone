5df4ab7d9f90a215f3b412057fe76083e6ee9b45
Jose Ramirez <josemramirez81@outlook.com>
2021-10-16 16:25:53 -0400

    Refactor code to send auth token and user information to MessageForm as props

--- client/src/pages/EventFormPage/EventFormPage.js
+++ client/src/pages/EventFormPage/EventFormPage.js
@@ -4,13 +4,13 @@ import {VendorForm} from '../../components/VendorForm/VendorForm';
 import {PaymentForm} from '../../components/PaymentForm/PaymentForm';
 import {MessageForm} from '../../components/MessageForm/MessageForm';
 
-const EventFormPage = ({ action, match, history, handleUpdate, authenticated, elementType }) => {
+const EventFormPage = ({ action, match, history, handleUpdate, authenticated, elementType, user }) => {
   return(
     <section className="form-section">
       {elementType === 'event' && (<EventForm action={(action ==="add")? "Add New" : "Edit"} history={history} match={match} handleUpdate={handleUpdate} authenticated={authenticated}/>)}
       {elementType === 'vendor' &&(<VendorForm action={(action ==="add")? "Add" : "Edit"} match={match} authenticated={authenticated} handleUpdate={handleUpdate}/>)}
       {elementType === 'vendorEvent' && (<PaymentForm match={match} authenticated={authenticated} handleUpdate={handleUpdate} />)}
-      {action === 'message' && <MessageForm />}
+      {action === 'message' && <MessageForm authenticated={authenticated} match={match} user={user}/>}
     </section>
   )
 }
