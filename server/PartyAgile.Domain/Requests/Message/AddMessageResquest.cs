9806080ad973c238ca1e60b778cfc55318e01988
Jose Ramirez <josemramirez81@outlook.com>
2021-10-16 16:08:08 -0400

    Refactor AddMessage DTO to include sender name

--- server/PartyAgile.Domain/Requests/Message/AddMessageResquest.cs
+++ server/PartyAgile.Domain/Requests/Message/AddMessageResquest.cs
@@ -9,6 +9,7 @@ namespace PartyAgile.Domain.Requests.Message
     public class AddMessageResquest
     {
         public string Content { get; set; }
+        public string Sender { get; set; }
         public DateTimeOffset Created { get; set; }
         public Guid VendorId { get; set; }
         public Guid EventId { get; set; }
