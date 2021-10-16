e9aee91db771f49fbf47c26517e9f2c2b104c528
Jose Ramirez <josemramirez81@outlook.com>
2021-10-16 16:28:39 -0400

    Refactor code to show the list of messages including sender name created and content

--- client/src/components/MessagesList/MessagesList.js
+++ client/src/components/MessagesList/MessagesList.js
@@ -1,12 +1,30 @@
 import './MessagesList.scss'
 import {Message} from '../Message/Message';
+import {useEffect, useRef} from 'react';
 
-export const MessagesList = () => {
+export const MessagesList = ({ messages, user }) => {
+  const listEndRef = useRef(null)
+
+  const scrollToEnd = () => {
+    listEndRef.current.scrollIntoView({ behavior: "smooth"})
+  }
+
+  useEffect(scrollToEnd, [messages])
+
   return(
     <ul className="message-list">
-      <Message owner='yours' />
-      <Message />
-      <Message  owner='yours' />
+      <div className="message-list__container">
+        <div ref={listEndRef}/>
+        {messages.map(message => {
+          return <Message
+            key={message.id}
+            owner={user && user.name === message.senderName ? 'yours' : ''}
+            content={message.content}
+            created={message.created}
+            name={message.senderName}
+          />
+        })}
+      </div>
     </ul>
   )
 }
