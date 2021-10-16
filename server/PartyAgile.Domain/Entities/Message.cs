71784c3e8f42ab92cbab05cfce74dedbf635499b
Jose Ramirez <josemramirez81@outlook.com>
2021-10-16 15:45:02 -0400

    Create Message entity

---
+++ server/PartyAgile.Domain/Entities/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyAgile.Domain.Entities
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+        public string SenderName { get; set; }
+        public Guid VendorId { get; set; }
+        public Guid EventId { get; set; }
+        
+        public VendorEvent Event { get; set; }
+    }
+}
