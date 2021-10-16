bdfa5cece93e0f8d8a76164839bece569bc5ef5e
Jose Ramirez <josemramirez81@outlook.com>
2021-10-15 23:39:05 -0400

    Refactor Message entity to include username as sender

---
+++ server/PartyAgile.API/Migrations/20211014165905_ModifyMessages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PartyAgile.API.Migrations
+{
+    public partial class ModifyMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sender",
+                schema: "partyagile",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderName",
+                schema: "partyagile",
+                table: "Messages",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.DropColumn(
+                name: "SenderName",
+                schema: "partyagile",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Sender",
+                schema: "partyagile",
+                table: "Messages",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+        }
+    }
+}
