c4ea8cccf95768abf1609cddf55bdced21eba31a
Jose Ramirez <josemramirez81@outlook.com>
2021-11-12 12:44:53 -0500

    Setup env variable for production

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const url = "https://localhost:5001/api/";
+export const url = process.env.REACT_APP_ENV === 'production'
+    ? 'https://api.partyagile.com/api' : "https://localhost:5001/api/"
